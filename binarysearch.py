ee1284c98c91620c6d9281d3d9062d611fab4df4
AishwaryaKalmangi <aishwarya.kalmangi009@gmail.com>
2022-01-24 23:02:09 -0500

    Binary Search in python

---
+++ binarysearch.py
@@ -0,0 +1,19 @@
+def binary_searchiter(arr, n):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < n:
+            low = mid + 1
+        elif arr[mid] > n:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+arr = [4, 7, 9, 16, 20, 25]
+n = 25
+r = binary_searchiter(arr, n)
+if r != -1:
+    print("Element found at index", str(r))
+else:
+    print("Element is not present in array")
